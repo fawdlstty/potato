50835ed655d0a0a2e15119aa4c60cd87f3dd528a
fawdlstty <f@fawdlstty.com>
2025-01-15 21:22:55 +0800

    remove example code

--- potato/src/main.rs
+++ potato/src/main.rs
@@ -11,9 +11,9 @@ async fn main() -> anyhow::Result<()> {
     let mut server = HttpServer::new("0.0.0.0:8080");
     server.configure(|ctx| {
         ctx.use_dispatch();
-        ctx.use_embedded_route("/", embed_dir!("assets/wwwroot"));
         ctx.use_doc("/doc/");
-        ctx.use_location_route("/", "/wwwroot");
+        //ctx.use_embedded_route("/", embed_dir!("assets/wwwroot"));
+        //ctx.use_location_route("/", "/wwwroot");
     });
     println!("visit: http://127.0.0.1:8080/doc/");
     server.serve_http().await
